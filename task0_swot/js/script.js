f757973b30ae616a7e8c77a46b28b3348d2c9215
Marc <marcf.eberhard@gmail.com>
2021-03-22 14:13:55 +0100

    changed color and content

--- task0_swot/js/script.js
+++ task0_swot/js/script.js
@@ -1,7 +1,7 @@
-let strength = ["Webentwicklung / Webdesign","WordPress und typo3 Development", "HTML","CSS / SCSS"]
-let weak = ["Wenig Erfahrung mit JavaScript", "Kaffee"]
-let treath = ["Fehlende Inhalte aufgrund der kurzen Zeit","määäh","muuuh", "wau"]
-let opp = ["Neues Wissen","Verschiedene Arten von Interfaces kennenlernen","Tiefer Einblick in den Aufbau von Interfaces"]
+let strength = ["Webentwicklung / Webdesign","WordPress und typo3 Development", "HTML","CSS / SCSS",]
+let weak = ["Wenig Erfahrung mit JavaScript", "Konzeption", "Kaffee"]
+let treath = ["Fehlende Inhalte aufgrund der kurzen Zeit"]
+let opp = ["Die Möglichkeit neues Wissen anzueignen","Verschiedene Arten von Interfaces kennenlernen","Tiefer Einblick in den Aufbau von Interfaces"]
 
 let strength_id = document.getElementById("strength_ul")
 let weakness_id = document.getElementById("weakness_ul")
