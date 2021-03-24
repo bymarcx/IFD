d06e6a6bb894e518905d6d4e6305c3cf15404225
Marc <marcf.eberhard@gmail.com>
2021-03-24 11:07:58 +0100

    content task0_swot

--- task0_swot/js/script.js
+++ task0_swot/js/script.js
@@ -1,5 +1,5 @@
-let strength = ["Webentwicklung / Webdesign","WordPress und typo3 Development", "HTML, CSS / SCSS"]
-let weak = ["Wenig Erfahrung mit JavaScript", "Konzeption", "Kaffee"]
+let strength = ["Webentwicklung / Webdesign", "Konzeption von Webseiten", "WordPress und typo3 Development", "HTML, CSS / SCSS",]
+let weak = ["Wenig Erfahrung mit JavaScript", "Konzeption von anderen Interface-Arten"]
 let treath = ["Fehlende Inhalte aufgrund der kurzen Zeit"]
 let opp = ["Die Möglichkeit neues Wissen anzueignen","Verschiedene Arten von Interfaces kennenlernen","Tiefer Einblick in den Aufbau von Interfaces"]
 
