dd75900892d6ec89e74564d152d46f3ba3fe4c2a
Marc <marcf.eberhard@gmail.com>
2021-06-19 12:28:17 +0200

    change task11 - vui

--- task11_vui-prototype/vui/js/playgroud-artyom-script.js
+++ task11_vui-prototype/vui/js/playgroud-artyom-script.js
@@ -4,34 +4,39 @@ window.addEventListener("load", function () {
     // init artyom
     var artyom = new Artyom();
     var name = "Marc";
+    var silent = false;
 
     document.getElementById("name").innerHTML = name;
+    document.getElementById("mode").innerHTML = "Silent mode: deaktiviert";
 
 
     // start artyom
     startContinuousArtyom();
 
+    // listen on button
     document.getElementById("activate").addEventListener("click", function () {
         toStart();
         document.getElementById("activate").style.display = "none";
     });
 
-    artyom.when("NOT_COMMAND_MATCHED", function () {
-        artyom.say("Wiederhole deine Anfrage bitte.");
+    artyom.redirectRecognizedTextOutput(function(recognized,isFinal){
+        if(isFinal){
+            document.getElementById("recognizedID").innerHTML = "Ich habe verstanden: <br><span>" + recognized+ "</span>";
+        }else{
+            document.getElementById("recognizedID").innerHTML = "Ich höre: <br><span>" + recognized+ "</span>";
+        }
     });
 
     // All catchable artyom errors will be catched with this
     artyom.when("ERROR", function (error) {
-        document.getElementById("error").style.display = "inline-block";
+        document.getElementById("error").style.display = "inline";
         if (error.code == "network") {
             document.getElementById("error").innerHTML = 'An error ocurred, artyom cannot work without internet connection !';
         }
-
-        if (error.code == "audio-capture") {
+        else if (error.code == "audio-capture") {
             document.getElementById("error").innerHTML = 'An error ocurred, artyom cannot work without a microphone';
         }
-
-        if (error.code == "not-allowed") {
+        else if (error.code == "not-allowed") {
             document.getElementById("error").innerHTML = 'An error ocurred, it seems the access to your microphone is denied';
         }
         else {
@@ -62,6 +67,11 @@ window.addEventListener("load", function () {
                 obeyKeyword: "Hallo Home",
             }).then(function () {
                 console.log("Ready!");
+
+                artyom.when("NOT_COMMAND_MATCHED", function () {
+
+                });
+
                 artyom.addCommands(initCommand);
                 document.getElementById("commands").innerHTML = 'Sage <br><br><span class="sayings">"Hallo Home"</span><br><br>oder drücke den Button!';
                 document.getElementById("activate").style.display = "inline-block";
@@ -75,18 +85,26 @@ window.addEventListener("load", function () {
         if (hour >= 0 && hour <= 12) {
             time = " Morgen ";
         } else if (hour >= 13 && hour <= 17) {
-            time = " Mittag ";
+            time = " Tag ";
         } else if (hour >= 18 && hour <= 24) {
             time = " Abend ";
         }
+        soundFeedback();
         artyom.say("Guten " + time + " " + name + ".");
         artyom.say("Was kann ich für dich tun?");
         displayFirstLevel();
+
+        artyom.when("NOT_COMMAND_MATCHED", function () {
+            artyom.say("Wiederhole deine Anfrage bitte.");
+        });
+
     }
 
     // BACK TO FIRST LEVEL FUNTION
     function backToFirstLevel() {
-        artyom.say("Kann ich sonst noch etwas für dich tun?");
+        if (!silent) {
+            artyom.say("Kann ich sonst noch etwas für dich tun?");
+        }
         displayFirstLevel();
     }
 
@@ -94,13 +112,18 @@ window.addEventListener("load", function () {
         artyom.emptyCommands();
         artyom.addCommands(firstLevelCommands);
         artyom.addCommands(endCommands);
-        document.getElementById("commands").innerHTML = 'Du kannst folgende Befehle ausführen <br><br><span class="sayings">"Ich heiße nicht ' + name + '!"<br>"Ist das {Gerät} noch an?"<br>"Schalte {Gerät} aus!"<br>"Schalte {Gerät} an!"<br>"Wie warm ist es im Raum {Raum}?"<br>"Stelle die Temperatur auf {Gradzahl}!"<br>"Ändere die Helligkeit auf {Prozentwert}!"<br>"Was sind meine Benachrichtigungen?"<br>"Neues Gerät verbinden!"</span><br><br> Beende das Gespräch mit <br><br> <span class="sayings">"Danke" </span> oder <span class="sayings">"Danke Home!"</span>';
+        document.getElementById("commands").innerHTML = 'Du kannst folgende Befehle ausführen <br><br><span class="sayings">"Ich heiße nicht ' + name + '!"<br>"Was ist der Silent Mode?"<br>"De-/aktiviere den Silent Mode!"<br>"Ist das [Gerät] noch an?"<br>"Schalte [Gerät] an/aus!"<br>"Wie warm ist es im Raum [Raum]?"<br>"Stelle die Temperatur auf [Gradzahl]!"<br>"Ändere die Helligkeit auf [Prozentwert]!"<br>"Was sind meine Benachrichtigungen?"<br>"Neues Gerät verbinden!"</span><br><br> Beende das Gespräch mit <br><br> <span class="sayings">"Danke" </span> oder <span class="sayings">"Danke Home!"</span>';
+    }
+
+    function soundFeedback() {
+        var audio = new Audio('confirm.wav');
+        audio.play();
     }
 
     // INIT COMMANDS
     var initCommand = [
         {
-            indexes: ["Hallo Home", "Hallo Hong", "Hallo Honk", "Home", "Hong", "Hallo"],
+            indexes: ["Hallo Home", "Hallo Hong", "Hallo Honk", "Home", "Hong", "Honk", "Hallo"],
             action: function (i) {
                 toStart();
                 document.getElementById("activate").style.display = "none";
@@ -117,7 +140,7 @@ window.addEventListener("load", function () {
                 artyom.say("Okay, wie soll ich dich denn in Zukunft nennen?");
                 artyom.emptyCommands();
                 artyom.addCommands(endCommands);
-                document.getElementById("commands").innerHTML = 'Antworte mit <br><br><span class="sayings">"Nenne mich {Name}" <br>"Abbrechen"</span> <br><br> Beende das Gespräch mit <br><br> <span class="sayings">"Danke"</span> oder <span class="sayings">"Danke Home!"</span>';
+                document.getElementById("commands").innerHTML = 'Antworte mit <br><br><span class="sayings">"Nenne mich [Name]" <br>"Abbrechen"</span> <br><br> Beende das Gespräch mit <br><br> <span class="sayings">"Danke"</span> oder <span class="sayings">"Danke Home!"</span>';
                 artyom.addCommands([
                     {
                         smart: true,
@@ -224,33 +247,54 @@ window.addEventListener("load", function () {
         },
         {
             smart: true,
-            indexes: ["Schalte * an", "aktiviere *"],
+            indexes: ["Schalte * an", "* an"],
             action: function (i, wildcard) {
-                artyom.say("Ich habe " + wildcard + " angeschaltet.");
+                if (silent) {
+                    soundFeedback();
+                }
+                else {
+                    artyom.say("Ich habe " + wildcard + " angeschaltet.");
+                }
                 backToFirstLevel();
             }
         },
         {
             smart: true,
-            indexes: ["Schalte * aus", "deaktiviere *"],
+            indexes: ["Schalte * aus", "* aus"],
             action: function (i, wildcard) {
-                artyom.say("Ich habe " + wildcard + " ausgeschaltet.");
+                if (silent) {
+                    soundFeedback();
+                }
+                else {
+                    artyom.say("Ich habe " + wildcard + " ausgeschaltet.");
+                }
                 backToFirstLevel();
             }
         },
         {
             smart: true,
-            indexes: ["Ändere die Helligkeit auf *",],
+            indexes: ["Ändere die Helligkeit auf *", "Helligkeit *"],
             action: function (i, wildcard) {
-                artyom.say("Ich habe die Helligkeit auf " + wildcard + " geändert.");
+                if (silent) {
+                    soundFeedback();
+                }
+                else {
+                    artyom.say("Ich habe die Helligkeit auf " + wildcard + " geändert.");
+
+                }
                 backToFirstLevel();
             }
         },
         {
             smart: true,
-            indexes: ["Stelle die Temperatur auf *",],
+            indexes: ["Stelle die Temperatur auf *", "Temperatur *"],
             action: function (i, wildcard) {
-                artyom.say("Ich habe die Temperatur auf " + wildcard + " gestellt.");
+                if (silent) {
+                    soundFeedback();
+                }
+                else {
+                    artyom.say("Ich habe die Temperatur auf " + wildcard + " gestellt.");
+                }
                 backToFirstLevel();
             }
         },
@@ -294,7 +338,7 @@ window.addEventListener("load", function () {
                                 artyom.say("Gut, welchem Raum möchtest du das Gerät hinzufügen?");
                                 artyom.emptyCommands();
                                 artyom.addCommands(endCommands);
-                                document.getElementById("commands").innerHTML = 'Sage <br><br><span class="sayings">"Füge es dem Raum {Raum} hinzu"</span> <br><br> Beende das Gespräch mit <br><br> <span class="sayings">"Danke"</span> oder <span class="sayings">"Danke Home!"</span>';
+                                document.getElementById("commands").innerHTML = 'Sage <br><br><span class="sayings">"Füge es dem Raum [Raum] hinzu"</span> <br><br> Beende das Gespräch mit <br><br> <span class="sayings">"Danke"</span> oder <span class="sayings">"Danke Home!"</span>';
                                 artyom.addCommands(
                                     [
                                         {
@@ -326,13 +370,101 @@ window.addEventListener("load", function () {
             }
         },
 
+        {
+            indexes: ["Was ist der Silent Mode",],
+            action: function (i) {
+                artyom.say("Wenn du den Silent Mode aktivierst werde ich dir nicht mehr bei jeder Anfrage eine Antwort geben. Ich werde dir stattdessen durch einen leisen Ton signalisieren dass ich deine Anfrage bearbeitet habe.");
+
+                if (silent) {
+                    artyom.say("Möchtest du den Silent Mode deaktivieren?");
+                }
+                else {
+                    artyom.say("Möchtest du den Silent Mode aktivieren?");
+                }
+
+                artyom.emptyCommands();
+                document.getElementById("commands").innerHTML = 'Antworte mit <br><br><span class="sayings">"Nein, danke." <br>"Ja, bitte."</span> <br><br> Beende das Gespräch mit <br><br> <span class="sayings">"Danke"</span> oder <span class="sayings">"Danke Home!"</span>';
+                artyom.addCommands(endCommands);
+                artyom.addCommands([
+                    {
+                        indexes: ["nein", "Nein danke"],
+                        action: function (i) {
+                            artyom.say("Okay.");
+                            backToFirstLevel()
+                        }
+                    },
+                    {
+                        indexes: ["Ja", "Ja bitte"],
+                        action: function (i) {
+                            if (silent) {
+                                artyom.say("Gut, Ich habe den Silent Mode für dich deaktiviert.");
+                            }
+                            else {
+                                artyom.say("Gut, Ich habe den Silent Mode für dich aktiviert.");
+                            }
+
+                            silent = true;
+                            document.getElementById("mode").innerHTML = "Silent mode: aktiviert";
+                            console.log("mode", silent);
+
+                            backToFirstLevel()
+                        }
+                    }
+                ]);
+
+            }
+        },
+
+        {
+            indexes: ["Aktiviere den Silent Mode",],
+            action: function (i, wildcard) {
+
+                soundFeedback();
+
+                silent = true;
+                document.getElementById("mode").innerHTML = "Silent mode: aktiviert";
+
+                backToFirstLevel();
+            }
+        },
+
+        {
+            indexes: ["Deaktiviere den Silent Mode",],
+            action: function (i, wildcard) {
+                if (silent) {
+                    artyom.say("Gut, Ich habe den Silent Mode für dich deaktiviert.");
+                }
+                else {
+                    artyom.say("Der Silent Mode ist aktuell schon ausgeschaltet.");
+                }
+                document.getElementById("mode").innerHTML = "Silent mode: deaktiviert";
+
+                silent = false;
+                backToFirstLevel();
+            }
+        },
+
+        {
+            indexes: ["Hallo Home", "Hallo *", "Hallo"],
+            action: function (i, wildcard) {
+                if (silent) {
+                    soundFeedback();
+                }
+                else {
+                }
+
+                backToFirstLevel();
+            }
+        },
+
+
     ];
 
     // END COMMANDS
     var endCommands = [
         {
             smart: true,
-            indexes: ["* danke", "danke *"],
+            indexes: ["danke *"],
             action: function (i, wildcard) {
                 console.log("Gespräch beendet!");
                 artyom.say("gerne! Melde dich bitte sobald ich etwas tun kann. Bis zum nächsten mal");
@@ -353,3 +485,72 @@ window.addEventListener("load", function () {
 
 });
 //# sourceMappingURL=playgroud-artyom-script.js.map
+
+
+
+
+
+
+
+
+
+
+window.onload = function () {
+    "use strict";
+    var paths = document.getElementsByTagName('path');
+    var visualizer = document.getElementById('visualizer');
+    var mask = visualizer.getElementById('mask');
+    var h = document.getElementsByTagName('h1')[0];
+    var path;
+    var report = 0;
+    
+    var soundAllowed = function (stream) {
+        //Audio stops listening in FF without // window.persistAudioStream = stream;
+        //https://bugzilla.mozilla.org/show_bug.cgi?id=965483
+        //https://support.mozilla.org/en-US/questions/984179
+        window.persistAudioStream = stream;
+        // h.innerHTML = "Thanks";
+        // h.setAttribute('style', 'opacity: 0;');
+        var audioContent = new AudioContext();
+        var audioStream = audioContent.createMediaStreamSource( stream );
+        var analyser = audioContent.createAnalyser();
+        audioStream.connect(analyser);
+        analyser.fftSize = 1024;
+
+        var frequencyArray = new Uint8Array(analyser.frequencyBinCount);
+        visualizer.setAttribute('viewBox', '0 0 255 255');
+      
+		//Through the frequencyArray has a length longer than 255, there seems to be no
+        //significant data after this point. Not worth visualizing.
+        
+        for (var i = 0 ; i < 255; i++) {
+            path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+            path.setAttribute('stroke-dasharray', '4,1');
+            mask.appendChild(path);
+        }
+        var doDraw = function () {
+            requestAnimationFrame(doDraw);
+            analyser.getByteFrequencyData(frequencyArray);
+          	var adjustedLength;
+            for (var i = 0 ; i < 255; i++) {
+              	adjustedLength = Math.floor(frequencyArray[i]) - (Math.floor(frequencyArray[i]) % 5);
+                paths[i].setAttribute('d', 'M '+ (i) +',255 l 0,-' + adjustedLength);
+            }
+
+        }
+        doDraw();
+    }
+
+    var soundNotAllowed = function (error) {
+        // h.innerHTML = "You must allow your microphone.";
+        console.log(error);
+    }
+
+    /*window.navigator = window.navigator || {};
+    /*navigator.getUserMedia =  navigator.getUserMedia       ||
+                              navigator.webkitGetUserMedia ||
+                              navigator.mozGetUserMedia    ||
+                              null;*/
+    navigator.getUserMedia({audio:true}, soundAllowed, soundNotAllowed);
+
+};
